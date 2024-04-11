a35f1b21f5e8b60ce484bdfeeaf93e2d4dcf8276
Alexandr Seleznev <alexandr.selezniov@outlook.com>
2024-04-11 23:01:40 +0500

    Fix: added setText and del close window

--- art/telebot.js
+++ art/telebot.js
@@ -4,23 +4,26 @@ tg.expand();
 tg.MainButton.textColor = "#FFFFFF";
 tg.MainButton.color = "#2cab37"
 
-document.getElementById('jsonData').addEventListener('input', () =>{
-    tg.MainButton.name = "Send";
+document.getElementById('jsonData').addEventListener('input', function () {
+    tg.MainButton.setText("Send");
     tg.MainButton.show();
 });
+
+Telegram.WebApp.onEvent.onClick
+
 Telegram.WebApp.onEvent("mainButtonClicked", function () {
-    const json = document.getElementById("jsonData").value;
-    let paramJson = [];
+        const json = document.getElementById("jsonData").value;
+        let paramJson = [];
 
-    for (let i = 0; i < x; i++) {
-        let param = document.getElementById("param-json_" + --x);
-        paramJson.push(param.value.trim());
-    }
+        for (let i = 0; i < x; i++) {
+            let param = document.getElementById("param-json_" + --x);
+            paramJson.push(param.value.trim());
+        }
 
-    let data = {
-        json : json,
-        paramJson : paramJson
+        let data = {
+            json: json,
+            paramJson: paramJson
+        }
+        tg.sendData(JSON.stringify(data));
     }
-    tg.sendData(JSON.stringify(data));
-    tg.close();
-});
+);
